449b5d2c2e8884cbc91eb8a2da12b8e7c6924bac
Manikanta Reddy S <manikanta.viswa@gmail.com>
2019-01-20 15:06:15 +0530

    Radio button styles updated

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ProfilePic from './ProfilePic';
 import { Link } from 'react-router-dom';
 
+const DEF_USER = {
+    name: '',
+    email: '',
+    gender: 'M',
+    phone: '',
+    photo: '',
+    photoVal: ''
+}
+
 export default class UserForm extends React.Component {
     state = {
-        user: {
-            name: '',
-            email: '',
-            gender: '',
-            phone: '',
-            photo: '',
-            photoVal: ''
-        }
+        user: { ...DEF_USER }
     };
 
     onSave(e) {
@@ -20,7 +22,7 @@ export default class UserForm extends React.Component {
     }
 
     componentDidMount() {
-        const {name, email, gender, phone} = this.props.user;
+        const { name = DEF_USER.name, email = DEF_USER.email, gender = DEF_USER.gender, phone = DEF_USER.phone } = this.props.user;
         this.setState({
             user: {
                 ...this.state.user,
@@ -50,34 +52,35 @@ export default class UserForm extends React.Component {
             }
         });
     }
-    
+
     render() {
         return (
             <form className="form pt-2">
                 <div className="row">
                     <div className="col-sm-6">
                         <div className="form-label-group">
-                            <input className="form-control" type="text" id="name" autoComplete="off" name="name" value={this.state.user.name} onChange={(e) => this.onChangeInput(e)} placeholder="Name"/>
+                            <input className="form-control" type="text" id="name" autoComplete="off" name="name" value={this.state.user.name} onChange={(e) => this.onChangeInput(e)} placeholder="Name" />
                             <label className="control-label" htmlFor="name">Name</label>
                         </div>
                         <div className="form-label-group">
-                            <input className="form-control" type="text" id="email" autoComplete="off" name="email" value={this.state.user.email} onChange={(e) => this.onChangeInput(e)} placeholder="Email"/>
+                            <input className="form-control" type="text" id="email" autoComplete="off" name="email" value={this.state.user.email} onChange={(e) => this.onChangeInput(e)} placeholder="Email" />
                             <label className="control-label" htmlFor="email">Email Id</label>
                         </div>
                         <div className="form-label-group">
-                            <input className="form-control" type="text" id="phone" autoComplete="off" name="phone" value={this.state.user.phone} onChange={(e) => this.onChangeInput(e)} placeholder="Phone"/>
+                            <input className="form-control" type="text" id="phone" autoComplete="off" name="phone" value={this.state.user.phone} onChange={(e) => this.onChangeInput(e)} placeholder="Phone" />
                             <label className="control-label" htmlFor="phone">Phone Number</label>
                         </div>
-                        <div className="form-label-group">
-                            <input className="form-control" type="text" id="gender" autoComplete="off" name="gender" value={this.state.user.gender} onChange={(e) => this.onChangeInput(e)} placeholder="Gender"/>
-                            <label className="control-label" htmlFor="gender">Gender</label>
+                        <div className="form-label-group radio-group">
+                            <label><input className="form-control-radio" type="radio" name="gender" value="M" checked={this.state.user.gender === 'M'} onChange={(e) => this.onChangeInput(e)} /> Male</label>
+                            <label><input className="form-control-radio" type="radio" name="gender" value="F" checked={this.state.user.gender === 'F'} onChange={(e) => this.onChangeInput(e)} /> Female</label>
+                            <label className="control-label">Gender</label>
                         </div>
                     </div>
                     <div className="col-sm-6">
                         <div className="form-label-group">
                             <input className="form-control" type="file" id="photo" name="photo" value={this.state.user.photo} onChange={(e) => this.onChangeFile(e)} />
                             <label className="control-label" htmlFor="photo">Profile Pic</label>
-                            <ProfilePic label="Existing Pic" user={this.props.user}/>
+                            <ProfilePic label="Existing Pic" user={this.props.user} />
                         </div>
                     </div>
                 </div>
